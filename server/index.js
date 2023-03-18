931a4661bfd4e4a94e76555def5e6d04c79a464e
ahmedhanora11 <ahmedhanora11@gmail.com>
2023-03-19 03:09:22 +0800

    Mongo DB Connection

--- server/index.js
+++ server/index.js
@@ -10,3 +10,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 //Mongo link here using cloud
+const CONNECTION_URL = 'mongodb+srv://ahmedhanora11:Dede7dede7.@cluster0.m4sqa1b.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+.then(() => app.listen(PORT, () => console.log(`The server is running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
