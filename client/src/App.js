23dd8087f1dc8beb3223032ba768f214637c5bb3
ahmedhanora11 <ahmedhanora11@gmail.com>
2023-04-27 23:46:35 +0800

    Pagination feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Container } from "@material-ui/core"
 
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 import dotenv from 'dotenv';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+
+    const user = JSON.parse(localStorage.getItem('profile'));
     return (
         <GoogleOAuthProvider clientId= "413738397596-6ukb2qqm27dquf9mv1s0n8kjej7af6ml.apps.googleusercontent.com" >
         <BrowserRouter>
-        <Container maxWidth= "lg" >
+        <Container maxWidth= "xl" >
             <Navbar />
             <Switch>
-                <Route path="/" exact component={Home} /> {/* Home Path */}
-                <Route path="/auth" exact component={Auth} /> {/* Auth Path */}
+                <Route path="/" exact component={() => <Redirect to="/posts" />} /> {/* Home Path */}
+                <Route path="/posts" exact component={Home} />
+                <Route path="/posts/search" exact component={Home} />
+                <Route path="/posts/:id" component={PostDetails} />
+                <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} /> {/* Auth Path */}
             </Switch>
         </Container>
         </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid } from "@material-ui/core"
+import { Container, Grow, Grid, Paper } from "@material-ui/core"
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import useStyles from "./styles"
+//Pagination
+import Pagination from "../Pagination";
+
 //Dispatch an action hook
 import { useDispatch } from "react-redux";
 //actions imports
@@ -27,6 +30,7 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4} >
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}><Pagination /></Paper>
                     </Grid>
                 </Grid>
             </Container>
